eac4ad7d2ddf6f344aa7f68b6c92de389f19f45d
dmitrijthedaddy <demitya7@gmail.com>
2021-07-05 20:24:06 +0500

    add -h/--help key

--- vulpa.py
+++ vulpa.py
@@ -4,14 +4,23 @@ from vulpa.parser import run
 
 argv = sys.argv
 
-if '--nogui' in argv:
+if '--nogui' in argv or '--help' in argv:
+    if '--help' in argv:
+        print('python vulpa.py --nogui -b <before-path> -a <after-path> [-d <xlsx-directory="reports">]')
+        print('    -b <path>:   путь к отчёту об уязвимостях "До"')
+        print('    -a <path>:   путь к отчёту об уязвимостях "После"')
+        print('    -d <path>:   (необязательно) путь к каталогу для сохранения выходных xlsx-файлов')
+        print('    -n, --nogui: вход в консольный режим')
+        print('    -h, --help:  эта справка\n')
+        sys.exit(0)
+
     bf_path = None
     try:
         bf_index = argv.index('-b')
         bf_path = argv[bf_index + 1]
     except ValueError:
         print('Неверно указан путь к файлу "До"')
-        print('Использование: python --nogui -b <before-path> -a <after-path> [-d <xlsx-directory="reports">]')
+        print('Использование: python vulpa.py --nogui -b <before-path> -a <after-path> [-d <xlsx-directory="reports">]')
         sys.exit(1)
 
     af_path = None
